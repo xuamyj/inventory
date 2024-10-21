16291ebb8a677f07f8f77cc18f021a23aec69cd9
Amy Xu <xuamyj@gmail.com>
2024-10-20 21:51:52 -0400

    finished sprite setup, starting naninovel setup

---
+++ Assets/Amy Scripts/PlayerController.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    /* ---- MOVEMENT ---- */
+    public InputAction LeftAction;
+    public InputAction RightAction;
+    public InputAction DownAction;
+    public InputAction UpAction;
+
+    public float speed;
+
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        /* ---- MOVEMENT ---- */
+
+        // QualitySettings.vSyncCount = 0;
+        // Application.targetFrameRate = 10;
+
+        LeftAction.Enable();
+        RightAction.Enable();
+        DownAction.Enable();
+        UpAction.Enable();
+
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /* ---- MOVEMENT ---- */
+
+        // UnityEngine.Debug.Log("Hi");
+        // UnityEngine.Debug.Log(Keyboard.current);
+        // UnityEngine.Debug
+        // Changed to "Both"
+        // added Input System Package, but maybe doesn't do anything
+        // added EventSystem and clicked twice, but maybe doesn't do anything
+
+        horizontal = 0.0f;
+        if (LeftAction.IsPressed())
+        {
+            horizontal = -speed;
+        }
+        else if (RightAction.IsPressed())
+        {
+            horizontal = speed;
+        }
+        // UnityEngine.Debug.Log("keyboard horizontal: " + horizontal);
+
+        vertical = 0.0f;
+        if (DownAction.IsPressed())
+        {
+            vertical = -speed;
+        }
+        else if (UpAction.IsPressed())
+        {
+            vertical = speed;
+        }
+        // UnityEngine.Debug.Log("keyboard vertical: " + vertical);
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position; // transform.position;
+        position.x = position.x + horizontal * Time.deltaTime;
+        position.y = position.y + vertical * Time.deltaTime;
+        // transform.position = position;
+        rigidbody2d.MovePosition(position);
+    }
+}
+
